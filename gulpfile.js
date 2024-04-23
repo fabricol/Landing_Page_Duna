2b7834d87f823c8915043db558524cb7d4971952
fabricol <fabricioctnmiranda@gmail.com>
2024-04-22 21:58:45 -0300

    Correção git caminho img 2x

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,7 @@ function styles() {
 function images() {
     return gulp.src('./src/img/**/*')
         .pipe(imagemin())
-        .pipe(gulp.dest('./dist/images'));
+        .pipe(gulp.dest('./dist/img'));
 }
 
 exports.default = gulp.parallel(styles, images);
