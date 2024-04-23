8655489e9965d1f8a58b988a90bdb4fad47c0069
fabricol <fabricioctnmiranda@gmail.com>
2024-04-22 22:50:55 -0300

    public

--- gulpfile.js
+++ gulpfile.js
@@ -5,13 +5,13 @@ const imagemin = require('gulp-imagemin');
 function styles() {
     return gulp.src('./src/styles/*.scss')
         .pipe(sass({outputStyle: 'compressed'}))
-        .pipe(gulp.dest('./dist/public/css')); // Update destination path to './public/css'
+        .pipe(gulp.dest('./dist/css'));
 }
 
 function images() {
     return gulp.src('./src/images/**/*')
         .pipe(imagemin())
-        .pipe(gulp.dest('./dist/public/images')); // Update destination path to './public/images'
+        .pipe(gulp.dest('./dist/images'));
 }
 
 exports.default = gulp.parallel(styles, images);
