4293c5b5cd8920b96f6cd53fe8493b61f6ca5f5b
fabricol <fabricioctnmiranda@gmail.com>
2024-04-22 22:14:20 -0300

    correção gulp 3

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,11 @@ function styles() {
         .pipe(gulp.dest('./dist/css'));
 }
 
+function images() {
+    return gulp.src('./src/img/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/img'));
+}
 
 exports.default = gulp.parallel(styles, images);
 
